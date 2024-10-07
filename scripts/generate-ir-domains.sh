4d4297f555d5270c97b3e7fd978e5f895b45e86b
t-e-s-tweb <King.Khanrocks.khan@gmail.com>
2024-10-07 18:43:44 +0500

    Update generate-ir-domains.sh

--- scripts/generate-ir-domains.sh
+++ scripts/generate-ir-domains.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gh release download -p domains.txt --repo bootmortis/iran-hosted-domains
-cat domains.txt | grep -Ev ".+\.ir$" | sed '1 a\ir\nxn--mgba3a4f16a' | LC_ALL=C sort -u > ir.txt
-comm -23 ir.txt ./redundant/redundant-domains.txt > ir-lite.txt
-mv ir.txt ir-lite.txt release
+gh release download -p domainspk.txt --repo t-e-s-tweb/v-rules
+cat domainspk.txt | grep -Ev ".+\.pk$" | sed '1 a\pk\nxn--mgbai9azgqp6j' | LC_ALL=C sort -u > pk.txt
+comm -23 pk.txt ./redundant/redundant-domains.txt > pk-lite.txt
+mv pk.txt pk-lite.txt release
