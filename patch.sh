97971712def07a2a289838e0c90e59fc7e245684
t-e-s-tweb <King.Khanrocks.khan@gmail.com>
2026-02-02 23:32:21 +0500

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -3,12 +3,12 @@ export GOMAXPROCS=$(nproc)\
 export CGO_ENABLED=1\
 export GOEXPERIMENT="runtimefreegc,sizespecializedmalloc,greenteagc,jsonv2,newinliner,heapminimum512kib"' \
 -e '/export CGO_CFLAGS="-target aarch64-linux-android21"/c\
-export CGO_CFLAGS="-target aarch64-linux-android21 -O3 -flto=thin -fomit-frame-pointer -fno-semantic-interposition -falign-functions=32"' \
+export CGO_CFLAGS="-target aarch64-linux-android21 -O2 -g"' \
 -e '/export CGO_CFLAGS="-target x86_64-linux-android21"/c\
-export CGO_CFLAGS="-target x86_64-linux-android21 -O3 -flto=thin -fomit-frame-pointer -fno-semantic-interposition -falign-functions=32"' \
+export CGO_CFLAGS="-target x86_64-linux-android21 -O2 -g"' \
 -e '/export CGO_CFLAGS="-target armv7a-linux-androideabi21"/c\
-export CGO_CFLAGS="-target armv7a-linux-androideabi21 -O3 -flto=thin -fomit-frame-pointer -fno-semantic-interposition -falign-functions=32"' \
+export CGO_CFLAGS="-target armv7a-linux-androideabi21 -O2 -g"' \
 -e '/export CGO_CXXFLAGS=/c\
-export CGO_CXXFLAGS="-O3 -flto=thin -fomit-frame-pointer -fno-semantic-interposition -falign-functions=32"' \
+export CGO_CXXFLAGS="-O2 -g"' \
 -e '/export CGO_LDFLAGS=/c\
-export CGO_LDFLAGS="-O3 -flto=thin -fuse-ld=lld -Wl,-O3 -Wl,--lto-O3 -Wl,-z,max-page-size=16384"' libxivpn/build.sh
+export CGO_LDFLAGS="-Wl,-z,max-page-size=0x4000 -Wl,-z,common-page-size=0x4000 -Wl,-z,separate-loadable-segments"' libxivpn/build.sh
